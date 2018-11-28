acfb360f0783398af9b2165a825c7aa732362f0b
AlejandraCP <alejandracp710@gmail.com>
2018-11-28 14:59:25 -0500

    create header

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = props => {
+  return (
+    <header className='container-header'>
+      <h1>
+        Cotizador de Seguro de Autos
+      </h1>
+    </header>
+  )
+}
+
+export default Header;
