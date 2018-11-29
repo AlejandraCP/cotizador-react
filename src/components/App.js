9b522ffa37362eabd899b2b1404112a9dcaa9c1e
AlejandraCP <alejandracp710@gmail.com>
2018-11-29 12:10:52 -0500

    'add-functions-to-get-result-with-year,-brand-and-plan'

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,31 @@
 import React, { Component } from 'react';
 import Header from './Header';
 import Formulario from './Formulario';
+import {obtenerDiferenciaAnio, calcularMarca, obtenerPlan} from './../Helper';
 
 class App extends Component {
 
   cotizarSeguroMet = (datosAuto) => {
 
     // aplicando destructutirng
-    const {marca, plan, año}= datosAuto;
-    console.log(plan);
+    const {marca, plan, year}= datosAuto;
+
+    let diferenciaAnio = obtenerDiferenciaAnio(year)
+
+    // precio base
+    let resultado = 2000;
+
+    // el resultado disminuye en 3% por cada año inferior
+    resultado -= ((diferenciaAnio * 3) * resultado) / 100;
+
+    // calcular resultado según marca
+    resultado = calcularMarca(marca) * resultado;
+
+    // según el plan el resultado debe aumentar
+    let choosePlan = obtenerPlan(plan);
+
+    resultado = choosePlan * resultado;
+    console.log(resultado);
     
   }
   render() {
